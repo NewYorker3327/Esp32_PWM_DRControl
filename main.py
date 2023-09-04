2a06472b336a67525de06f77aea0b9aeea54e877
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-09-04 11:27:04 -0300

    Updated songs.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from machine import Pin, PWM, ADC
 from machine import SoftI2C as 	I2C
 import dht
 import _thread
+from math import log2
 
 #Visor:
 from i2c_lcd import I2cLcd
@@ -12,14 +13,21 @@ from i2c_lcd import I2cLcd
 from ds18x20 import DS18X20
 from onewire import OneWire
 
+def voltar(k, log2 = log2, limite_inf = 440):
+    return int(12*log2(k/limite_inf))
+
+def passar_nota(n):
+    return int(220 * 2 ** (n/12))
+
 def musicas(mus:str):
     global pwm, modo_global
     obj = pwm
 
     backup = modo_global
     modo_global == "musica"
-    temp = 8
+    
     if mus == "intro":
+        temp = 8
         mus = [[392, 200, 2], [392, 400, 3], [523, 200, 2], [523, 400, 3],
                [588, 200, 2], [588, 400, 3], [660, 200, 2], [660, 400, 3], [392, 200, 2],[392, 400, 23], [392,0, .8],
                [392, 400, 5], [523, 400, 5], [523, 400, 3], [588, 400, 5],
@@ -27,25 +35,51 @@ def musicas(mus:str):
                [660, 400, 3], [588, 400, 3], [523, 400, 25], [392, 10, 3]]
 
     if mus == "exit":
+        temp = 8
         mus = [[1568+784, 440, 2], [1175+587, 440, 2], [784+523, 440, 2], [587+294, 440, 2],
                [523+392, 440, 2]]
 
     if mus == "zelda":
-        mus = [[665, 200, 6], [498, 200, 6], [665, 200, 4], [746, 200, 2],
-               [791, 200, 2], [888, 200, 2], [996, 200, 16], [1118, 200, 2],
-               [1255, 200, 16], [1118, 200, 2], [996, 200, 2], [1118, 200, 4],
-               [996, 200, 2], [1118, 200, 4], [996, 200, 2], [940, 200, 8],
-               [838, 200, 4], [888, 200, 4], [1056, 200, 2], [940, 200, 2],
-               [888, 200, 2], [791, 200, 4], [888, 200, 2], [996, 200, 8],
-               [888, 200, 2], [791, 200, 2], [746, 200, 4], [838, 200, 2],
-               [888, 200, 8], [1118, 200, 8], [996, 200, 8]]
+        v = 300
+        temp = 16
+        mus = [[12, v, 6], [0, v, 10], [2, v, 2], [3, v, 2], [5, v, 2], [7, v, 16],
+               [8, v, 2], [10, v, 2], [12, v, 16], [10, v, 2], [8, v, 2], [10, v, 4],
+               [8, v, 2], [7, v, 16], [5, v, 4], [7, v, 2], [8, v, 16], [7, v, 2], [5, v, 2], [3, v, 4],
+               [5, v, 2], [7, v, 16], [5, v, 2], [3, v, 2], [2, v, 2], [3, v, 2], [5, v, 2], [8, v, 6], [7, v, 12]]
+
+    if mus == "fef":
+        v = 300
+        temp = 16
+        mus = [[1, v, 2], [8, v, 2], [15, v, 2], [16, v, 6], [1, v, 2], [8, v, 2], [15, v, 2], [16, v, 6], [1, v, 2], [8, v, 2], [15, v, 2], [16, v, 6],
+               [[1,8], v, 2], [8, v, 2], [[15, 8], v, 2], [[16, 8], v, 2], [16, v, 4],
+               [[-1,8], v, 2], [8, v, 2], [[15, 8], v, 2], [[16, 8], v, 2], [16, v, 4],
+               [[-3,8], v, 2], [8, v, 2], [[15, 8], v, 2], [[16, v, 8], 2], [16, v, 4],
+               [[3,6], v, 4], [[1, 4], v, 4], [[-1, 2], v, 4],
+               [1, v, 8], [-1, v, 2], [6, v, 2], [1, v, 8], [-1, v, 2], [8, v, 2],
+               [4, v, 8], [8, v, 2], [6, v, 2], [8, v, 8]]
 
     for nota in mus:
-        globals()['freq'] = nota[0]
-        globals()['potValueReal'] = nota[1]
-        obj.freq(nota[0])
-        obj.duty(nota[1])
-        sleep(nota[2]/temp)
+        if type(nota[0]) == list:
+            resp = 0
+            for n_individual in nota[0]:
+                resp += passar_nota(n_individual)
+            globals()['freq'] = resp
+            globals()['potValueReal'] = nota[1]
+            obj.freq(resp)
+            obj.duty(nota[1])
+            sleep(nota[2]/temp)
+        elif nota[0] < 30:
+            globals()['freq'] = passar_nota(nota[0])
+            globals()['potValueReal'] = nota[1]
+            obj.freq(passar_nota(nota[0]))
+            obj.duty(nota[1])
+            sleep(nota[2]/temp)
+        else:
+            globals()['freq'] = nota[0]
+            globals()['potValueReal'] = nota[1]
+            obj.freq(nota[0])
+            obj.duty(nota[1])
+            sleep(nota[2]/temp)
 
     modo_global = backup
 
@@ -202,7 +236,7 @@ def interface():
             if seta.read() < 4095/3:
                 m_ = "intro"
             elif seta.read() < 4095/3 * 2:
-                m_ = "exit"
+                m_ = "fef"
             else:
                 m_ = "zelda"
 
